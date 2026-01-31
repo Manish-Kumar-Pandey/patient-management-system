b25d8fd4af7ba3bbd911ae682b4adb2a74845fd0
Manish Kumar Pandey <diwakarpandey767@gmail.com>
2026-01-31 12:12:04 +0530

    first commit

---
+++ patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
@@ -0,0 +1,64 @@
+package com.pm.patient_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+    @NotBlank
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of Birth is required")
+    private String dateOfBirth;
+
+    @NotBlank(message = "Register date is required")
+    private String registeredDate;
+
+    public @NotBlank @Size(max = 100, message = "Name cannot exceed 100 characters") String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank @Size(max = 100, message = "Name cannot exceed 100 characters") String name) {
+        this.name = name;
+    }
+
+    public @NotBlank @Email(message = "Email should be valid") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank @Email(message = "Email should be valid") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "Address is required") String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotBlank(message = "Address is required") String address) {
+        this.address = address;
+    }
+
+    public @NotBlank(message = "Date of Birth is required") String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(@NotBlank(message = "Date of Birth is required") String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public @NotBlank(message = "Register date is required") String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(@NotBlank(message = "Register date is required") String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+}
---
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -0,0 +1,42 @@
+package com.pm.patient_service.service;
+
+import com.pm.patient_service.dto.PatientRequestDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.exception.EmailAlreadyExistsException;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    private static final Logger log = LoggerFactory.getLogger(PatientService.class);
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+
+        return patients.stream().map(
+                PatientMapper::toDTO
+        ).toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmailIgnoreCase(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A patient with this email already exists"+" "+ patientRequestDTO.getEmail());
+        }
+        log.info("Total patients in DB before = {}", patientRepository.count());
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        log.info("Total patients in DB after= {}", patientRepository.count());
+        return PatientMapper.toDTO(newPatient);
+    }
+
+}
